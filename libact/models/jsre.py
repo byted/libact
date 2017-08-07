80b5dbf226abaf740665f7be34eacd2f6c3a4062
Stefan Selent <sselent@babbel.com>
2017-08-07 13:12:36 +0200

    refactor jSRE code and remove tmp files

--- libact/models/jsre.py
+++ libact/models/jsre.py
@@ -26,10 +26,13 @@ class JSRE(ProbabilisticModel):
 
     """
 
-    def __init__(self, model_path, jsre_path, tmp_dir='/tmp', cache_size=1024, n_gram=3, window_size=2, C=None, max_memory=2048, kernel='SL'):
+    def __init__(self, jsre_path, tmp_dir='/tmp', cache_size=1024, n_gram=3, window_size=2, C=None, max_memory=2048, kernel='SL'):
         """model_path => where to save the model to"""
         self.tmp_dir = tmp_dir
-        self.model = model_path
+        self.model = self.__get_tmp_filename('model')
+        self.tmp_training_file = self.__get_tmp_filename('train')
+        self.tmp_test_file = self.__get_tmp_filename('test')
+        self.tmp_output_file = self.__get_tmp_filename('output')
         self.classpath = './bin:./lib/*'
         self.jsre_path = jsre_path
         self.max_memory = '-mx{}M'.format(max_memory)
@@ -61,38 +64,37 @@ class JSRE(ProbabilisticModel):
             sys.exit(-1)
 
     def __raw_predict(self, instances):
-        ts = self.__generate_timestamp()
-        tmp_example_file = self.__get_tmp_filename('test')
-        tmp_output_file = self.__get_tmp_filename('output')
-
-        with open(tmp_example_file, 'w') as testf:
+        with open(self.tmp_test_file, 'w') as testf:
             for instance in instances:
                 # use 1 as default label b/c -1 will crash jSRE even tough thats the proposed label for unknown
                 testf.write('1\t{}\n'.format(instance[0]))  # all features are encoded as one for jSRE
 
         cmd = self.predict_template.format(
             cp=self.classpath, memory=self.max_memory, model=self.model,
-            to_predict=tmp_example_file, output=tmp_output_file)
+            to_predict=self.tmp_test_file, output=self.tmp_output_file)
 
-        raw_prediction, _ = self.__run_command(cmd, tmp_output_file)
+        raw_prediction, _ = self.__run_command(cmd, self.tmp_output_file)
         splitted_lines = (line.split('\t') for line in raw_prediction[1:])
         predictions, probas = zip(*[(int(float(row[0])), [float(n) for n in row[2:]]) for row in splitted_lines])
+        try:
+            os.remove(self.tmp_test_file)
+            os.remove(self.tmp_output_file)
+        except OSError:
+            pass
         return predictions, probas
 
     def train(self, dataset, *args, **kwargs):
-        ts = self.__generate_timestamp()
-        tmp_training_file = self.__get_tmp_filename('train')
         lines = ['{}\t{}'.format(lbl, feat) for feat, lbl in zip(*dataset.format_jsre())]
-        with open(tmp_training_file, 'w') as trainingf:
+        with open(self.tmp_training_file, 'w') as trainingf:
             trainingf.write('\n'.join(lines))
 
         cmd = self.train_template.format(
             cp=self.classpath, memory=self.max_memory, model_output=self.model,
-            to_train=tmp_training_file, jsre_paras=self.jsre_paras)
+            to_train=self.tmp_training_file, jsre_paras=self.jsre_paras)
 
         self.__run_command(cmd)
         try:
-            os.remove(tmp_training_file)
+            os.remove(self.tmp_training_file)
         except OSError:
             pass
 
@@ -106,15 +108,13 @@ class JSRE(ProbabilisticModel):
         return np.array(probas)
 
     def score(self, dataset, *args, **kwargs):
-        tmp_test_file = self.__get_tmp_filename('test')
-        tmp_output_file = self.__get_tmp_filename('output')
         lines = ['{}\t{}'.format(lbl, feat) for feat, lbl in zip(*dataset.format_jsre())]
-        with open(tmp_test_file, 'w') as trainingf:
+        with open(self.tmp_test_file, 'w') as trainingf:
             trainingf.write('\n'.join(lines))
 
         cmd = self.predict_template.format(
             cp=self.classpath, memory=self.max_memory, model=self.model,
-            to_predict=tmp_test_file, output=tmp_output_file)
+            to_predict=self.tmp_test_file, output=self.tmp_output_file)
         _, stdout = self.__run_command(cmd)
 
         match = re.search(r'Accuracy = (\d+\.\d+)%', stdout)
@@ -124,8 +124,8 @@ class JSRE(ProbabilisticModel):
 
         accuracy = float(match.groups()[0])/100
         try:
-            os.remove(tmp_test_file)
-            os.remove(tmp_output_file)
+            os.remove(self.tmp_test_file)
+            os.remove(self.tmp_output_file)
         except OSError:
             pass
 
